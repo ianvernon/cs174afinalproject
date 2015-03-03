27725695ed4a38e0a8fdd4e2392ad2112bbd2e48
Ian Vernon <ianvernon92@gmail.com>
2015-03-03 14:20:12 -0800

    added InsuranceCompany class

---
+++ src/edu/ucsb/cs/InsuranceCompany.java
@@ -0,0 +1,16 @@
+package edu.ucsb.cs;
+
+/**
+ * Created by ianvernon on 3/3/15.
+ */
+public class InsuranceCompany
+{
+    private int payerID;
+    private String name;
+
+    public InsuranceCompany(int payerID, String name)
+    {
+        this.payerID = payerID;
+        this.name = name;
+    }
+}
